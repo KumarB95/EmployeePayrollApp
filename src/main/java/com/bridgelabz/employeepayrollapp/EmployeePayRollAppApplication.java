b4031941002c26ecf2d2e509428eac59d72d810f
KumarB95 <kumarbalingal@gmail.com>
2021-12-17 22:16:54 +0530

    Refactored : CreateAppPropertiesAndGettingOutputAtApplog

--- src/main/java/com/bridgelabz/employeepayrollapp/EmployeePayRollAppApplication.java
+++ src/main/java/com/bridgelabz/employeepayrollapp/EmployeePayRollAppApplication.java
@@ -3,14 +3,19 @@ package com.bridgelabz.employeepayrollapp;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.ApplicationContext;
 
 @SpringBootApplication
 @Slf4j
 public class EmployeePayRollAppApplication {
 
     public static void main(String[] args) {
-        System.out.println("Welcome to Emp payroll");
-        SpringApplication.run(EmployeePayRollAppApplication.class, args);
+        ApplicationContext context =SpringApplication.run(EmployeePayRollAppApplication.class, args);
+        log.info("Employee Payroll App Started in {} Environment",
+                context.getEnvironment().getProperty("environment"));
+        log.info("Employee Payroll DB User is {}",
+                context.getEnvironment().getProperty("spring.datasource.username"));
+        log.info("Test message of Slf4j");
     }
 
 }
