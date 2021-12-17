6283d913175554f8440180a47ef20c70c811dbb2
KumarB95 <kumarbalingal@gmail.com>
2021-12-17 21:06:08 +0530

    Refactored : used slf4j for logging details in console

--- src/main/java/com/bridgelabz/employeepayrollapp/EmployeePayRollAppApplication.java
+++ src/main/java/com/bridgelabz/employeepayrollapp/EmployeePayRollAppApplication.java
@@ -1,8 +1,9 @@
 package com.bridgelabz.employeepayrollapp;
 
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-
+@Slf4j
 @SpringBootApplication
 public class EmployeePayRollAppApplication {
 
