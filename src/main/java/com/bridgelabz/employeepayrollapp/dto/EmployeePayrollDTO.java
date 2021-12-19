716a571877a2f5c5ce8b36755c70e5a00a901ffd
KumarB95 <kumarbalingal@gmail.com>
2021-12-19 21:50:34 +0530

    Added:Validation For all Feilds in EmployeePayroll Dto

--- src/main/java/com/bridgelabz/employeepayrollapp/dto/EmployeePayrollDTO.java
+++ src/main/java/com/bridgelabz/employeepayrollapp/dto/EmployeePayrollDTO.java
@@ -4,29 +4,29 @@ import com.fasterxml.jackson.annotation.JsonFormat;
 import lombok.Data;
 import lombok.ToString;
 
-import javax.validation.constraints.Min;
-import javax.validation.constraints.Pattern;
+import javax.validation.constraints.*;
 import java.time.LocalDate;
 import java.util.List;
 
 @Data
 public @ToString class  EmployeePayrollDTO {
-
-
     @Pattern(regexp = "^[A-Z]{1}[a-zA-Z\\s]{2,}$", message = "Employee Name is invalid")
     public String name;
 
     @Min(value = 500, message = "Min Wage should be more than 500")
     public long salary;
-
+    @Pattern(regexp = "male|female", message = " Gender  needs to be male or female")
     public String gender;
 
-    @JsonFormat(pattern="dd MMM yyyy")
+    @JsonFormat(pattern = "dd MMM yyyy")
+    @NotNull(message = "Startdate should not be empty")
+    @PastOrPresent(message = "startDate shuld be past or todays date")
     public LocalDate startDate;
-
+    @NotBlank(message = "Note can not be empty")
     public String note;
-
+    @NotBlank(message = "profilePic can not be empty")
     public String profilePic;
-
+    @NotNull(message = "department should not be empty")
     public List<String> departments;
+
 }
